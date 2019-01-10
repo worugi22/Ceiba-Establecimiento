46ec014ad8b44dad8697ebe5faf539d17cfea2bf
wolsey.rubio <wolsey.rubio@WOLSEY-RUBIO.ceiba.local>
2019-01-10 11:36:50 -0500

    segundo ejemplo

--- src/main/java/com/ceiba/model/Vigilante.java
+++ src/main/java/com/ceiba/model/Vigilante.java
@@ -1,5 +1,10 @@
 package com.ceiba.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Table
+@Entity(name="vigilante")
 public class Vigilante {
 	private int idVigilante;
 }
