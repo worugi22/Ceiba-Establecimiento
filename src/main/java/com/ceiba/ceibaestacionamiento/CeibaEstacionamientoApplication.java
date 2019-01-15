c5392a7e18146ee461cf025ef97d10ef297b699f
wolsey.rubio <wolsey.rubio@WOLSEY-RUBIO.ceiba.local>
2019-01-15 10:59:35 -0500

    funcionando correctamente

--- src/main/java/com/ceiba/ceibaestacionamiento/CeibaEstacionamientoApplication.java
+++ src/main/java/com/ceiba/ceibaestacionamiento/CeibaEstacionamientoApplication.java
@@ -8,6 +8,7 @@ public class CeibaEstacionamientoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CeibaEstacionamientoApplication.class, args);
+		System.out.println("Funcionando");
 	}
 
 }
